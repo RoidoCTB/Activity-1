2ae85783508dacad77f8d888dbc6112acd801e53
RoidoCTB <116477695+RoidoCTB@users.noreply.github.com>
2025-08-08 21:42:35 +0800

    Update server.js
    removed duplicate code

--- server.js
+++ server.js
@@ -3,14 +3,11 @@ const app = express();
 const PORT = 5000;
 const data = require("./data.json");
 
-app.listen(PORT, function () {
-console.log('https://localhost:${PORT}');
-});
-
 app.get("/api/profile", function (req, res) {
     res.json(data);
 });
 
 app.listen(PORT, function () {
     console.log(`http://localhost:${PORT}`);
+
 });
